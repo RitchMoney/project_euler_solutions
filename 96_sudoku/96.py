4bd59072a420bb4309cb73b2ebab27517436f44a
RitchMoney <rddanna@gmail.com>
2025-01-31 10:26:30 -0800

    ?

--- 96_sudoku/96.py
+++ 96_sudoku/96.py
@@ -8,21 +8,22 @@ for line in txt_file:
 
 txt_file.close()
 
-count = 0
+puzzle_number = 0
 line_count = 0
 grid_dict = {}
 
 # parse into lists of lines in dictionary by id number
 for line in line_list:
     if "Grid" in line:
-        count += 1
+        puzzle_number += 1
 
-        grid_dict[count] = []
+        grid_dict[puzzle_number] = []
     else:
+        grid_dict[puzzle_number][line_count] = []
         for char in line:
-            grid_dict[count][line_count] = []
-            grid_dict[count][line_count].append(char)
+            grid_dict[puzzle_number][line_count].append(char)
             line_count += 1
 
         line_count = 0
 
+print(grid_dict[1])
